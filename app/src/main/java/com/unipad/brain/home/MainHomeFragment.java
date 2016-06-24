a6ef823ef43bd282f9ce841d307b87230739daeb
gongkana <395992592@qq.com>
2016-06-24 20:01:52 +0800

    gongkan

--- app/src/main/java/com/unipad/brain/home/MainHomeFragment.java
+++ app/src/main/java/com/unipad/brain/home/MainHomeFragment.java
@@ -23,12 +23,7 @@ import com.unipad.http.HitopNewsList;
 public class MainHomeFragment extends MainBasicFragment {
 
     private FrameLayout mLayoutHome;
-    private TextView txt_back;
-    private TextView txt_showmain;
-    private TextView txt_flod;
-    private TextView txt_time;
-    private TextView txt_power;
-    private TextView txt_wifi;
+
 //    private IntroductionFragment introductionFragment;
     private Fragment mLeftFragment;
 
@@ -56,25 +51,6 @@ public class MainHomeFragment extends MainBasicFragment {
         //frame 组件
 //        fl_homepager = (FrameLayout) mActivity.findViewById(R.id.fl_mainpager_info);
         initHomeFragment();
-
-        //返回键
-        txt_back = (TextView) mActivity.findViewById(R.id.tv_title_back_left);
-        //主菜单组件
-        txt_showmain = (TextView) mActivity.findViewById(R.id.tv_title_mainmenu_show);
-        //折叠显示
-        txt_flod = (TextView) mActivity.findViewById(R.id.tv_title_main_flod_show);
-        //当前时间
-        txt_time = (TextView) mActivity.findViewById(R.id.tv_current_time_text);
-        //电量
-        txt_power = (TextView) mActivity.findViewById(R.id.tv_title_battery_power_icon);
-        //wifi标示
-        txt_wifi = (TextView) mActivity.findViewById(R.id.tv_title_wifi_icon);
-
-
-
-        txt_back.setOnClickListener(this);
-        txt_showmain.setOnClickListener(this);
-        txt_flod.setOnClickListener(this);
 
     }
 
