484ef6ad2917aa6d8ad0d5722a70850e3775da46
gongkana <395992592@qq.com>
2016-06-27 16:26:53 +0800

    huangzujia

---
+++ app/src/main/java/com/unipad/brain/personal/PersonalInfoActivty.java
@@ -0,0 +1,13 @@
+package com.unipad.brain.personal;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PersonalInfoActivty extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_info_activty);
+    }
+}
