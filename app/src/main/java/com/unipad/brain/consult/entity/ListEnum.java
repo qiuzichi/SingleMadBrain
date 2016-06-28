20be3996550d8322a4c067f21d13e5d0c958710f
liuxiang <759106474@qq.com>
2016-06-28 15:45:12 +0800

    liuxiang

--- app/src/main/java/com/unipad/brain/consult/entity/ListEnum.java
+++ app/src/main/java/com/unipad/brain/consult/entity/ListEnum.java
@@ -9,11 +9,19 @@ public enum ListEnum{
     CONSULT_PRESENT(R.string.consult_present, R.drawable.news_present),
     COMPETITION_PRESENT(R.string.competition_present, R.drawable.occasions_pesent);
 
-    int nameResId;
-    int labelResId;
+    private int nameResId;
+    private int labelResId;
 
     private ListEnum(int nameResId, int labelResId){
         this.nameResId = nameResId;
         this.labelResId = labelResId;
+    }
+
+    public int getNameResId(){
+        return  nameResId;
+    }
+
+    public int getLabelResId(){
+        return  labelResId;
     }
 }
