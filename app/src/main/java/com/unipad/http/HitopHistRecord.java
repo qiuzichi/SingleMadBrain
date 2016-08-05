7fbb56af08f58e45e05c9203f0d8f81abfd1410c
huangzujia <374660180@qq.com>
2016-08-05 10:28:27 +0800

    huangzujia

--- app/src/main/java/com/unipad/http/HitopHistRecord.java
+++ app/src/main/java/com/unipad/http/HitopHistRecord.java
@@ -28,11 +28,11 @@ public class HitopHistRecord extends HitopRequest<List<HisRecord>>{
             mParams.addBodyParameter("status", "" + status);
         }
         if (null != startTime) {
-            startTime=startTime.replaceAll("/","|");
+            startTime.replaceAll("/","|");
             mParams.addBodyParameter("startDate", startTime);
         }
         if (null != endDate) {
-            endDate=endDate.replaceAll("/","|");
+            endDate.replaceAll("/","|");
             mParams.addBodyParameter("endDate", endDate);
         }
         mParams.addBodyParameter("page", "" + page);
